944ccb0e3667b5a6480f46f631f5e450fba5a922
nourhan <nourhanibrahim2004@gmail.com>
2023-04-03 15:40:45 -0700

    task0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _memset - Sets the first num bytes of the block of memory pointed by ptr
+ * @s: starting address of memory
+ * @b: the desired value
+ * @n: number of bytes to be changed
+ *
+ * Return: changed array with new value for n bytes
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int x = 0;
+
+	for (; n > 0; x++)
+	{
+		s[x] = b;
+		n--;
+	}
+	return (s);
+}
