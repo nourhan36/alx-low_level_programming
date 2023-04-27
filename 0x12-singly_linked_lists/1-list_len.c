fe3cdfde83531d821739c90eed6cc2b001459865
nourhan <nourhanibrahim2004@gmail.com>
2023-04-27 12:40:33 -0700

    list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * list_len - number of elements in the linked list
+ * @ptr: pointer
+ *
+ * Return: number of elements
+ */
+size_t list_len(const list_t *ptr)
+{
+size_t num = 0;
+
+while (ptr)
+{
+n++;
+ptr = ptr->next;
+}
+return (num);
+}
